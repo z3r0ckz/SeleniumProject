dbf9f7c94267ff9d90d1cd9888963bfec7499d43
Luis Martinez <luismf.1492@gmail.com>
2023-03-13 22:23:27 -0600

    ver 1 of Branch1

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,65 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.Objects;
 
 public class Main {
+
+    static String WebPage = "";
+    static String LoginTitle = "";
+    static String NoAccess = "";
+    static boolean LoadingStatus;
+    static boolean LoginStatus;
+
     public static void main(String[] args) {
+        //Setup Language EN
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("lang=en-GB");
+
         //1 Step  ---- Open the Chrome Browser
         System.setProperty("webdriver.chrome.driver","JarFiles/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
 
         //Navigate to the URL
         driver.get("https://store.steampowered.com/");
-        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+        WebPage = driver.getCurrentUrl();
+
 
         //Step 2 Click login button
-        driver.findElement(By.linkText("iniciar sesión")).click();
+        driver.findElement(By.linkText("login")).click();
+        System.out.println(driver.getTitle());
+        LoginTitle = driver.getTitle();
 
         //Step 3 Input Random Strings as credentials
         //Enter UserName
-        driver.findElement(By.className("newlogindialog_TextInput_2eKVn")).clear();
         driver.findElement(By.className("newlogindialog_TextInput_2eKVn")).sendKeys("hELLOO");
 
         //Enter Password
-        driver.findElement(By.className("newlogindialog_FieldLabel_3d8dp")).clear();
-        driver.findElement(By.className("newlogindialog_FieldLabel_3d8dp")).sendKeys('PAASSWORD');
+        driver.findElement(By.className("newlogindialog_FieldLabel_3d8dp")).sendKeys("PAASSWORD");
 
         //Click on Log in
         driver.findElement(By.className("newlogindialog_SubmitButton_2QgFE")).click();
 
         //Loading element displayed if access was correct
-        driver.findElement(By.className("throbber_topCircle_3znUF")).wait(1000);
+        driver.findElement(By.className("throbber_topCircle_3znUF"));
+        LoadingStatus = driver.findElement(By.className("throbber_topCircle_3znUF")).isDisplayed();
 
         //Wrong user or password
         System.out.println(driver.findElement(By.className("newlogindialog_FormError_1Mcy9")).getText());
-
-        //Wait until element apperas
-
+        NoAccess = (driver.findElement(By.className("newlogindialog_FormError_1Mcy9")).getText());
+        LoginStatus = Objects.equals(NoAccess, "Please check your password and account name and try again");
 
         //Close the browser
-        //driver.close();
+        driver.close();
 
+    }
 
-
+    public void AutoTest(){
 
 
     }
+
 }
--- src/TestCase.java
+++ src/TestCase.java
@@ -1,80 +1,56 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 public class TestCase {
-<<<<<<< HEAD
 
-  public static String WebPage = "";
-  public static String LoginTitle = "";
-  public static boolean LoadingStatus;
-  public static boolean LoginStatus;
-  //Run Test Case in TestNG and using Asserts
-    @Test
-    //Main page is displayed
-    private void NavigateMainPage(String WebPage){
-      System.out.println("Running the first test");
-      Assert.assertEquals(WebPage,"https://store.steampowered.com/");
-=======
-  //Run Test Case in TestNG and using Asserts
+    static String WebPage = "";
+    static String LoginTitle = "";
+    static boolean LoadingStatus;
+    static boolean LoginStatus;
+    //Run Test Case in TestNG and using Asserts
     @Test
     //Main page is displayed
     void NavigateMainPage(String Title){
-      Assert.assertEquals(Title,"https://store.steampowered.com/");
->>>>>>> a54ca32d70a54397dd4955b4bd96b149da349d21
-      System.out.println("This line is executed because assertEquals "
-              + "passed since both the strings are same");
+        Assert.assertEquals(Title,"https://store.steampowered.com/");
+        System.out.println("This line is executed because assertEquals "
+                + "passed since both the strings are same");
     }
 
     @Test
-  //Login Page is open
-<<<<<<< HEAD
-    private void LoginPage(String LoginTitle){
-      System.out.println("Running the second test");
-=======
+        //Login Page is open
     void LoginPage(String LoginTitle){
->>>>>>> a54ca32d70a54397dd4955b4bd96b149da349d21
-      Assert.assertEquals(LoginTitle,"Sign In");
-      System.out.println("This line is executed because assertEquals "
-              + "passed since both the strings are same");
+        Assert.assertEquals(LoginTitle,"Sign In");
+        System.out.println("This line is executed because assertEquals "
+                + "passed since both the strings are same");
     }
 
     @Test
     //Loading element is displayed
-<<<<<<< HEAD
-    private void ElementAppears(boolean Status){
-      System.out.println("Running the third test");
-      Assert.assertTrue(Status, String.valueOf(true));
-=======
     public void LoadingElement(boolean Status){
-      Assert.assertTrue(Status,true);
->>>>>>> a54ca32d70a54397dd4955b4bd96b149da349d21
-      System.out.println("This line will be executed if the element appear");
+        Assert.assertTrue(Status,true);
+        System.out.println("This line will be executed if the element appear");
     }
 
     @Test
-  //Error text in login credentials is displayed
+    //Error text in login credentials is displayed
     public void LoadingElement(boolean LoginStatus){
-<<<<<<< HEAD
-      System.out.println("Running the fourth test");
-      Assert.assertTrue(LoginStatus, String.valueOf(true));
-      System.out.println("This line will be executed if there are wrong credentials");
+        Assert.assertTrue(LoginStatus,true);
+        System.out.println("This line will be executed if there are wrong credentials");
     }
 
-  public void getWebPage() {
-  }
-  public void getLoginTitle() {
-  }
-
-  public void isLoadingStatus() {
-  }
-
-  public void isLoginStatus() {
-  }
-=======
-      Assert.assertTrue(LoginStatus,true);
-      System.out.println("This line will be executed if there are wrong credentials");
+    public static String getWebPage() {
+        return WebPage;
     }
 
+    public static String getLoginTitle() {
+        return LoginTitle;
+    }
 
->>>>>>> a54ca32d70a54397dd4955b4bd96b149da349d21
+    public static boolean isLoadingStatus() {
+        return LoadingStatus;
+    }
 
+    public static boolean isLoginStatus() {
+        return LoginStatus;
+    }
 }
+
